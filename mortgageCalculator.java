04444a013bc72ce3d76b368efe620831ce6a94de
Nabeel <abdulnabeel40@gmail.com>
2022-02-16 10:28:04 +0500

    updated mortgageCaclculator

--- mortgageCalculator.java
+++ mortgageCalculator.java
@@ -1,21 +1,19 @@
-package com.company;
-
 import java.text.NumberFormat;
 import java.util.Scanner;
 
-public class Main {
+public class MortgageCalculator {
 
     public static void main(String[] args) {
         // program which calculates mortgage payment
         final byte MONTHS_IN_YEAR = 12;
         final byte PERCENT = 100;
         // taking user inputs
-        Scanner scan = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
         float principal;
         do {
             System.out.print("Principal: ");
-            principal = scan.nextFloat();
+            principal = scanner.nextFloat();
             if (!(principal >= 1000 && principal <= 1000000))
                 System.out.println("Enter a number between 1,000 and  1,000,000.");
         } while (!(principal >= 1000 && principal <= 1000000));
@@ -23,7 +21,7 @@ public class Main {
         float annualInterest;
         do {
             System.out.print("Annual Interest: ");
-            annualInterest = scan.nextFloat();
+            annualInterest = scanner.nextFloat();
             if (!(annualInterest > 0 && annualInterest <= 30))
                 System.out.println("Enter a value greater than 0 and less than or equal to 30");
         } while (!(annualInterest > 0.0 && annualInterest <= 30));
@@ -31,10 +29,12 @@ public class Main {
         byte timePeriod;
         do {
             System.out.print("Period (Years) Interest: ");
-            timePeriod = scan.nextByte();
+            timePeriod = scanner.nextByte();
             if (!(timePeriod > 0 && timePeriod <= 30))
                 System.out.println("Enter a value between 1 and 30");
         } while (!(timePeriod > 0 && timePeriod <= 30));
+
+        scanner.close();
 
         float monthlyInterestRate = annualInterest / (MONTHS_IN_YEAR * PERCENT);
         int numberOfPayments = timePeriod * MONTHS_IN_YEAR;
