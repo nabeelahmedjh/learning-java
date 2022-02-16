b2f9e84fbe447a7317f931e7fd0bd54f7c9b34f7
Nabeel <abdulnabeel40@gmail.com>
2022-02-16 19:22:05 +0500

    updated UserGreet program

--- UserGreet.java
+++ UserGreet.java
@@ -1,13 +1,40 @@
 import java.nio.Buffer;
 import java.util.Scanner;
 
+import javax.sound.sampled.SourceDataLine;
+
 public class UserGreet {
 
     public static void main(String[] args) {
-        System.out.print("Your Name: ");
+        // Program calculator
+
         Scanner scan = new Scanner(System.in);
-        String name = scan.nextLine();
-        System.out.println("Hello, " + name);
+
+        int num1, num2;
+        String op;
+        System.out.println("a: ");
+        num1 = scan.nextInt();
+        System.out.println("b: ");
+        num2 = scan.nextInt();
+
+        System.out.println("OP: ");
+        op = scan.next();
+
         scan.close();
+
+        switch (op) {
+            case "+":
+                System.out.println(num1 + num2);
+                break;
+            case "-":
+                System.out.println(num1 - num2);
+                break;
+            case "*":
+                System.out.println(num1 * num2);
+                break;
+            case "/":
+                System.out.println((float) num1 / num2);
+                break;
+        }
     }
 }
