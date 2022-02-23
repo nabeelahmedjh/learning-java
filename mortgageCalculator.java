22097defd9866e5d9ec05fa39148b1f31bf7b3a8
Nabeel <abdulnabeel40@gmail.com>
2022-02-23 12:53:38 +0500

    Updated mortgageCalculator

--- mortgageCalculator.java
+++ mortgageCalculator.java
@@ -5,46 +5,62 @@ public class MortgageCalculator {
 
     public static void main(String[] args) {
         // program which calculates mortgage payment
-        final byte MONTHS_IN_YEAR = 12;
-        final byte PERCENT = 100;
+
         // taking user inputs
-        Scanner scanner = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
 
         float principal;
-        do {
+
+        while (true) {
             System.out.print("Principal: ");
-            principal = scanner.nextFloat();
-            if (!(principal >= 1000 && principal <= 1000000))
-                System.out.println("Enter a number between 1,000 and  1,000,000.");
-        } while (!(principal >= 1000 && principal <= 1000000));
+            principal = scan.nextFloat();
+            if (principal >= 1000 && principal <= 1000000)
+                break;
+
+            System.out.println("Enter a number between 1,000 and  1,000,000.");
+        }
 
         float annualInterest;
-        do {
+
+        while (true) {
             System.out.print("Annual Interest: ");
-            annualInterest = scanner.nextFloat();
-            if (!(annualInterest > 0 && annualInterest <= 30))
-                System.out.println("Enter a value greater than 0 and less than or equal to 30");
-        } while (!(annualInterest > 0.0 && annualInterest <= 30));
+            annualInterest = scan.nextFloat();
+            if (annualInterest > 0 && annualInterest <= 30)
+                break;
+
+            System.out.println("Enter a value greater than 0 and less than or equal to 30");
+        }
 
         byte timePeriod;
-        do {
-            System.out.print("Period (Years) Interest: ");
-            timePeriod = scanner.nextByte();
-            if (!(timePeriod > 0 && timePeriod <= 30))
-                System.out.println("Enter a value between 1 and 30");
-        } while (!(timePeriod > 0 && timePeriod <= 30));
 
-        scanner.close();
+        while (true) {
+            System.out.print("Period (year) Interest: ");
+            timePeriod = scan.nextByte();
+            if (timePeriod > 0 && timePeriod <= 30)
+                break;
+            System.out.println("Enter a value between 1 and 30");
+        }
+
+        scan.close();
+
+        double mortgage = calculateMortgage(principal, annualInterest, timePeriod);
+        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+        System.out.println("Mortgage: " + mortgageFormatted);
+    }
+
+    public static double calculateMortgage(
+            float principal,
+            float annualInterest,
+            int timePeriod) {
+
+        final byte MONTHS_IN_YEAR = 12;
+        final byte PERCENT = 100;
 
         float monthlyInterestRate = annualInterest / (MONTHS_IN_YEAR * PERCENT);
         int numberOfPayments = timePeriod * MONTHS_IN_YEAR;
 
-        // formula for calculating mortgage payment
-        float mortgage = (float) Math.pow((1 + monthlyInterestRate), numberOfPayments);
-        mortgage /= (Math.pow((1 + monthlyInterestRate), numberOfPayments) - 1);
-        mortgage *= principal * monthlyInterestRate;
-
-        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
-        System.out.println("Mortgage: " + mortgageFormatted);
+        double mortgage = (principal * monthlyInterestRate) * (Math.pow((1 + monthlyInterestRate), numberOfPayments)
+                / ((Math.pow((1 + monthlyInterestRate), numberOfPayments) - 1)));
+        return mortgage;
     }
 }
