0de35f9aff4841b2ff3d749ff76b9c49c2362f8b
Nabeel <abdulnabeel40@gmail.com>
2022-03-05 00:30:29 +0500

    Program that will replace a substring in a string

---
+++ StringReplace.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class RegularExpression {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("String: ");
+        String userInput = scan.nextLine();
+        System.out.print("To Replace: ");
+        String toReplace = scan.next();
+
+        System.out.print("New Value: ");
+        String newValue = scan.next();
+        System.out.println(userInput.replaceAll(toReplace, newValue));
+
+        scan.close();
+    }
+}
