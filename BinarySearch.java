0c390d928378d0361a4af1f5159804ee17606e4d
Nabeel <abdulnabeel40@gmail.com>
2022-03-23 20:55:50 +0500

    Binary Search in java

---
+++ BinarySearch.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class BinarySearch {
+    public static void main(String args[]) {
+        int arr[] = { 1, 3, 546, 76, 787, 65, 445, 76, 45, -32, -65, -54, 32, 0 };
+        int arrLen = arr.length, userInput;
+        Scanner scan = new Scanner(System.in);
+        userInput = scan.nextInt();
+        scan.close();
+        binarySearch(arr, arrLen, userInput);
+
+    }
+
+    public static void binarySearch(int arr[], int arrLen, int toFind) {
+
+        int start = 0, end = arrLen - 1, mid;
+
+        while (start < end) {
+            mid = start + end / 2;
+
+            if (toFind > arr[mid]) {
+                start = mid + 1;
+            } else if (toFind < arr[mid]) {
+                end = mid - 1;
+            } else {
+                System.out.println("Found at index: " + mid);
+                break;
+            }
+        }
+        System.out.println("Not Found!");
+
+    }
+
+}
