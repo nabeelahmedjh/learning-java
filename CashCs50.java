e11f001d481fd95a50f25c6cfe42b23f7c5c66b9
Nabeel <abdulnabeel40@gmail.com>
2022-03-24 09:41:11 +0500

    implimented cs50 cash in java

--- CashCs50.java
+++ CashCs50.java
@@ -3,20 +3,30 @@ import java.util.Scanner;
 public class CashCs50 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        float dollar = 0f;
+        int cents, change = 0;
 
         while (true) {
             System.out.print("Change owed: ");
-            dollar = scan.nextFloat();
-            if (dollar > 0)
+            cents = scan.nextInt();
+            if (cents > 0)
                 break;
         }
         scan.close();
-        float cents = dollar * 100;
-
         while (cents > 0) {
 
+            if (cents >= 25)
+                cents -= 25;
+            else if (cents >= 10)
+                cents -= 10;
+            else if (cents >= 5)
+                cents -= 5;
+            else
+                cents -= 1;
+
+            change++;
+
         }
+        System.out.println("Change owed: " + change);
     }
 
 }
