5b61cd6b87da2acfc5d294fff4f0042a2bc00ec0
nabeelahmedjh <abdulnabeel40@gmail.com>
2022-06-07 12:25:22 +0500

    added calender

---
+++ CalenderTest.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class CalenderTest {
+
+    public static void main(String[] args) {
+        Calendar c = Calendar.getInstance();
+
+        System.out.println(c.getTime());
+    }
+
+}
